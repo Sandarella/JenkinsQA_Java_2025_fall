9cb5451fab4075acfa62bff9f1d98deb6f92ea24
Lev Eydelkind <eydelkindl@gmail.com>
2026-01-12 22:48:50 +0200

    Refactor HomePage (to Page Factory): adding FindBy, refactoring methods, removing duplicate methods, renaming usages in tests, correcting typos...

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -2,7 +2,6 @@ package school.redrover;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.page.HomePage;
@@ -194,7 +193,6 @@ public class PipelineTest extends BaseTest {
         Assert.assertEquals(actualHomePageHeading, expectedHomePageHeading);
     }
 
-    @Ignore
     @Test(dependsOnMethods = "testCreateNewPipeline", dataProvider = "validAliases")
     public void testScheduleWithValidData(String validTimePeriod) {
 
