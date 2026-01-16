36d7cefabcbd006d999e8796c059c5e21e99cdd9
Elena-Galina <e.lena.galina@yandex.ru>
2026-01-16 14:49:58 +0300

    AT_18.005.01-Freestyle Project Management - Workspace

--- src/test/java/school/redrover/FreestyleProjectTest.java
+++ src/test/java/school/redrover/FreestyleProjectTest.java
@@ -302,12 +302,14 @@ public class FreestyleProjectTest extends BaseTest {
         Assert.assertTrue(selectedParameterList.contains(parameterName));
     }
 
-    @Test(dependsOnMethods = {"testCreate", "testScheduleBuild"})
+    @Test(dependsOnMethods = "testCreate")
     public void testNavigationToWorkspaceAfterBuild() {
         final String expectedHeadingText = "Workspace of " + PROJECT_NAME + " on Built-In Node";
 
         String actualHeadingText = new HomePage(getDriver())
                 .openProject(PROJECT_NAME, new FreestyleProjectStatusPage(getDriver()))
+                .getSidebarComponent()
+                .clickSidebarBuildNow()
                 .getSidebarComponent()
                 .clickSidebarWorkspace()
                 .getHeader()
