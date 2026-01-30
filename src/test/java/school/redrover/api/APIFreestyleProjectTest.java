bf1e6212c1cd1520727083387cc9f4f9b1dd8e21
Marin4kin <marinkatik1@gmail.com>
2026-01-30 23:21:39 +0300

    new testCreateUser

--- src/test/java/school/redrover/api/APIFreestyleProjectTest.java
+++ src/test/java/school/redrover/api/APIFreestyleProjectTest.java
@@ -115,7 +115,7 @@ public class APIFreestyleProjectTest extends APIBaseTest {
     @Test
     public void testCreateUser() {
         Map<String, String> userForm = new HashMap<>();
-        userForm.put("username", "UserUser" );
+        userForm.put("username", "UserUser");
         userForm.put("password1", "passwordUser");
         userForm.put("password2", "passwordUser");
         userForm.put("fullname", "UserUserName");
